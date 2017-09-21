f56abc1e462c6f67c4ed32b09ab68b7eb0fe59e1
Robert Frydenlund <robert_frydenlund@msn.com>
2017-09-21 23:36:52 +0200

    Preparing for 2.0.0.0. Changed to SpotifyAPI-NET. Using local API for playback information, and web API to change playback states.

---
+++ SpotifyPlugin/Parent.cs
@@ -0,0 +1,63 @@
+﻿using SpotifyAPI.Local;
+using SpotifyAPI.Local.Models;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System.Diagnostics;
+using System.Threading;
+using System;
+
+namespace SpotifyPlugin
+{
+    public class Parent
+    {
+        public StatusResponse Status { get { return status; } }
+        private StatusResponse status;
+
+        public bool SpotifyIsRunning;
+        
+        private SpotifyLocalAPI LocalAPI;
+        public SpotifyWebAPI WebAPI;
+        Process Spotify;
+
+        public Parent()
+        {
+            LocalAPI = new SpotifyLocalAPI();
+            LocalAPI.Connect();
+            WebAPIFactory waf = new WebAPIFactory("http://127.0.0.1", 7476, "82910a2ee5b34426b1a87e7c672e2284", SpotifyAPI.Web.Enums.Scope.UserReadPlaybackState | SpotifyAPI.Web.Enums.Scope.UserModifyPlaybackState);
+            // TODO blocking
+            
+            Thread t = new Thread(() => WebAPI = waf.GetWebApi().Result);
+            t.Start();
+
+            
+            System.Timers.Timer timer = new System.Timers.Timer(50);
+            timer.Elapsed += Timer_Elapsed;
+            timer.AutoReset = true;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            status = LocalAPI.GetStatus();
+            if (status == null) return;
+
+        }
+
+        public bool Continue()
+        {
+            if (Spotify != null && !Spotify.HasExited) return true;
+
+            var procs = Process.GetProcessesByName("Spotify");
+
+            if (procs.Length < 1) return false;
+
+            Spotify = procs[0];
+            return true;
+        }
+
+        internal void Reconnect()
+        {
+            LocalAPI.Connect();
+        }
+    }
+}
--- SpotifyPlugin/SpotifyPlugin.cs
+++ SpotifyPlugin/SpotifyPlugin.cs
@@ -1,7 +1,7 @@
 ﻿using Rainmeter;
 using System;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SpotifyPlugin
 {
@@ -9,9 +9,7 @@ namespace SpotifyPlugin
     {
         public static string coverPath = "";
         public static string defaultPath = "";
-
-        bool DEBUG = false;
-        Status Current_Status;
+        
         int numDecimals = 0;
 
         enum MeasureType
@@ -46,12 +44,13 @@ namespace SpotifyPlugin
             CoverPath,
             Data
         }
-
         private MeasureType Type = MeasureType.Running;
+        
+        Parent parent;
 
-        public Measure()
+        public Measure(Parent parent)
         {
-
+            this.parent = parent;
         }
 
         public void Reload(Rainmeter.API rm, ref double maxValue)
@@ -63,10 +62,6 @@ namespace SpotifyPlugin
             {
                 case "data":
                     Type = MeasureType.Data;
-                    break;
-                case "debug":
-                    EnableDebugMode(rm.ReadInt("Verbosity", 0));
-                    Type = MeasureType.DEBUG;
                     break;
                 case "trackuri":
                     Type = MeasureType.TrackURI;
@@ -151,112 +146,133 @@ namespace SpotifyPlugin
 
         internal double Update()
         {
-            //API.Log(API.LogType.Error, StatusControl.Current_Status.rawData);
-            // Update status
-            Current_Status = StatusControl.Current_Status;
-            switch (Type)
+            if (!parent.Continue()) return 0.0;
+            
+            if (parent.Status == null)
             {
-                case MeasureType.DEBUG:
-                    return 0;
-                case MeasureType.Repeat:
-                    return Current_Status.repeat ? 1 : 0;
+                //Rainmeter.API.Log(API.LogType.Notice, "Status null");
+                return 0.0;
+            }
 
-                case MeasureType.Shuffle:
-                    return Current_Status.shuffle ? 1 : 0;
+            try
+            {
+                switch (Type)
+                {
+                    case MeasureType.DEBUG:
+                        return 0;
+                    case MeasureType.Repeat:
+                        return parent.Status.Repeat ? 1 : 0;
 
-                case MeasureType.PositionSeconds:
-                    return Current_Status.playing_position;
+                    case MeasureType.Shuffle:
+                        return parent.Status.Shuffle ? 1 : 0;
 
-                case MeasureType.Playing:
-                    return Current_Status.playing ? 1 : 0;
+                    case MeasureType.PositionSeconds:
+                        return parent.Status.PlayingPosition;
 
-                case MeasureType.LengthSeconds:
-                    return Current_Status.track.length;
+                    case MeasureType.Playing:
+                        return parent.Status.Playing ? 1 : 0;
 
-                case MeasureType.Progress:
-                    return Current_Status.playing_position / (Current_Status.track.length * 1.00);
+                    case MeasureType.LengthSeconds:
+                        return parent.Status.Track.Length;
 
+                    case MeasureType.Progress:
+                        return parent.Status.PlayingPosition / parent.Status.Track.Length;
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                parent.Reconnect();
+                Rainmeter.API.Log(API.LogType.Warning, e.Message);
             }
             return 0.0;
         }
 
-        private void EnableDebugMode(int i)
-        {
-            if (DEBUG == true) return;
-
-            DEBUG = true;
-            AllocConsole();
-            Console.WriteLine("Console window activated!");
-        }
-
-        // CONSOLE
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool AllocConsole();
-
-
         public string GetString()
         {
-            // Update status
-            Current_Status = StatusControl.Current_Status;
-            switch (Type)
+            if (!parent.Continue()) return "";
+
+            if (parent.Status == null)
             {
+                //Rainmeter.API.Log(API.LogType.Notice, "Status null");
+                return "";
+            }
+            try
+            {
+                switch (Type)
+                {
+                    case MeasureType.TrackURI:
+                        //return this.Current_Status.track.track_resource.uri;
+                        return parent.Status.Track.TrackResource.Uri;
 
-                case MeasureType.TrackURI:
-                    return this.Current_Status.track.track_resource.uri;
+                    case MeasureType.AlbumURI:
+                        //return this.Current_Status.track.album_resource.uri;
+                        return parent.Status.Track.AlbumResource.Uri;
 
-                case MeasureType.AlbumURI:
-                    return this.Current_Status.track.album_resource.uri;
+                    case MeasureType.ArtistURI:
+                        //return this.Current_Status.track.artist_resource.uri;
+                        return parent.Status.Track.ArtistResource.Uri;
 
-                case MeasureType.ArtistURI:
-                    return this.Current_Status.track.artist_resource.uri;
+                    case MeasureType.Progress:
+                        //return ((Current_Status.playing_position / Current_Status.track.length) * 100).ToString();//"##0"
+                        //return ((Current_Status.playing_position / Current_Status.track.length) * 100).ToString("N"+numDecimals);//"##0"
+                        return "err";
 
-                case MeasureType.Progress:
-                    //return ((Current_Status.playing_position / Current_Status.track.length) * 100).ToString();//"##0"
-                    return ((Current_Status.playing_position / Current_Status.track.length) * 100).ToString("N"+numDecimals);//"##0"
+                    case MeasureType.Volume:
+                        //double volume = 100 * Current_Status.volume;
+                        //return volume.ToString("##0");
+                        return (parent.Status.Volume * 100).ToString();
 
-                case MeasureType.Volume:
-                    double volume = 100 * Current_Status.volume;
-                    return volume.ToString("##0");
+                    case MeasureType.Position:
+                        double playingPosition = parent.Status.PlayingPosition;
+                        double sec = Math.Floor(playingPosition % 60);
+                        double min = Math.Floor(playingPosition / 60);
+                        return String.Format("{0}:{1}", min.ToString("#00"), sec.ToString("00"));
 
-                case MeasureType.Position:
-                    double playingPosition = Current_Status.playing_position;
-                    double sec = Math.Floor(playingPosition % 60);
-                    double min = Math.Floor(playingPosition / 60);
-                    return String.Format("{0}:{1}", min.ToString("#00"), sec.ToString("00"));
+                    case MeasureType.Length:
+                        double trackLength = parent.Status.Track.Length;
+                        double secl = Math.Floor(trackLength % 60);
+                        double minl = Math.Floor(trackLength / 60);
+                        return String.Format("{0}:{1}", minl.ToString("#00"), secl.ToString("00"));
 
-                case MeasureType.Length:
-                    double trackLength = Current_Status.track.length;
-                    double secl = Math.Floor(trackLength % 60);
-                    double minl = Math.Floor(trackLength / 60);
-                    return String.Format("{0}:{1}", minl.ToString("#00"), secl.ToString("00"));
+                    case MeasureType.TrackName:
+                        //return Current_Status.track.track_resource.name;
+                        return parent.Status.Track.TrackResource.Name;
+                    case MeasureType.ArtistName:
+                        return parent.Status.Track.ArtistResource.Name;
 
-                case MeasureType.TrackName:
-                    return toUTF8(Current_Status.track.track_resource.name);
+                    // TODO
+                    case MeasureType.AlbumArt60:
+                        return AlbumArt.getArt(parent.Status.Track.AlbumResource.Uri, 60, defaultPath, coverPath);
+                    //return StatusControl.getArt(60, defaultPath, coverPath);
 
-                case MeasureType.ArtistName:
-                    return toUTF8(Current_Status.track.artist_resource.name);
+                    case MeasureType.AlbumArt85:
+                        return AlbumArt.getArt(parent.Status.Track.AlbumResource.Uri, 85, defaultPath, coverPath);
+                    //return StatusControl.getArt(85, defaultPath, coverPath);
 
-                case MeasureType.AlbumArt60:
-                    return StatusControl.getArt(60, defaultPath, coverPath);
+                    case MeasureType.AlbumArt120:
+                        return AlbumArt.getArt(parent.Status.Track.AlbumResource.Uri, 120, defaultPath, coverPath);
+                    //return StatusControl.getArt(120, defaultPath, coverPath);
 
-                case MeasureType.AlbumArt85:
-                    return StatusControl.getArt(85, defaultPath, coverPath);
+                    case MeasureType.AlbumArt300:
+                        return AlbumArt.getArt(parent.Status.Track.AlbumResource.Uri, 300, defaultPath, coverPath);
+                    //return StatusControl.getArt(300, defaultPath, coverPath);
 
-                case MeasureType.AlbumArt120:
-                    return StatusControl.getArt(120, defaultPath, coverPath);
+                    case MeasureType.AlbumArt640:
+                        return AlbumArt.getArt(parent.Status.Track.AlbumResource.Uri, 640, defaultPath, coverPath);
+                    //return StatusControl.getArt(640, defaultPath, coverPath);
 
-                case MeasureType.AlbumArt300:
-                    return StatusControl.getArt(300, defaultPath, coverPath);
+                    case MeasureType.AlbumName:
+                        return parent.Status.Track.AlbumResource.Name;
 
-                case MeasureType.AlbumArt640:
-                    return StatusControl.getArt(640, defaultPath, coverPath);
-
-                case MeasureType.AlbumName:
-                    return toUTF8(Current_Status.track.album_resource.name);
-
-                case MeasureType.CoverPath:
-                    return StatusControl.CoverPath;
+                    case MeasureType.CoverPath:
+                        throw new NotImplementedException();
+                        //return StatusControl.CoverPath;
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                parent.Reconnect();
+                Rainmeter.API.Log(API.LogType.Warning, e.Message);
             }
 
             // MeasureType.Major, MeasureType.Minor, and MeasureType.Number are
@@ -266,15 +282,65 @@ namespace SpotifyPlugin
             return null;
         }
 
-        private string toUTF8(string s)
+        internal void ExecuteBang(string arg)
         {
-            byte[] bytes = Encoding.Default.GetBytes(s);
-            return Encoding.UTF8.GetString(bytes);
-        }
-
-        internal void ExecuteBang(string p)
-        {
-            //throw new NotImplementedException(p);
+            if (!parent.Continue()) return;
+            
+            if (parent.WebAPI == null)
+            {
+                Rainmeter.API.Log(API.LogType.Warning, "Command failed, no web api connected");
+                return;
+            }
+            else if (parent.Status == null)
+            {
+                Rainmeter.API.Log(API.LogType.Warning, "Cant connect to Spotify");
+                return;
+            }
+            string[] args = Regex.Split(arg, " ");
+            switch (args[0].ToLowerInvariant())
+            {
+                case "playpause":
+                    if (parent.Status.Playing)
+                    {
+                        goto case "pause";
+                    }
+                    else
+                    {
+                        goto case "play";
+                    }
+                case "play":
+                    parent.WebAPI.ResumePlayback();
+                    //webApi.Play();
+                    break;
+                case "pause":
+                    //webApi.Pause();
+                    parent.WebAPI.PausePlayback();
+                    break;
+                case "next":
+                    //webApi.Next();
+                    parent.WebAPI.SkipPlaybackToNext();
+                    break;
+                case "previous":
+                    // TODO always skips to previous, should probably seek to 0 if progress > threshold
+                    parent.WebAPI.SkipPlaybackToPrevious();
+                    break;
+                case "volume":
+                    parent.WebAPI.SetVolume(Int32.Parse(args[1]));
+                    break;
+                case "seek":
+                    parent.WebAPI.SeekPlayback(Int32.Parse(args[1]));
+                    break;
+                case "seekpercent":
+                    parent.WebAPI.SeekPlayback((parent.Status.Track.Length * Int32.Parse(args[1])) / 100);
+                    break;
+                case "shuffle":
+                    parent.WebAPI.SetShuffle(bool.Parse(args[1]));
+                    break;
+                case "repeat":
+                    parent.WebAPI.SetRepeatMode((SpotifyAPI.Web.Enums.RepeatState)Enum.Parse(typeof(SpotifyAPI.Web.Enums.RepeatState), args[1]));
+                    break;
+            }
+            API.Log(API.LogType.Warning, $"Unknown command: {arg}");
         }
     }
 
@@ -282,10 +348,13 @@ namespace SpotifyPlugin
     {
         static IntPtr StringBuffer = IntPtr.Zero;
 
+        static Parent parent;
+
         [DllExport]
         public static void Initialize(ref IntPtr data, IntPtr rm)
         {
-            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure()));
+            if (parent == null) { parent = new Parent(); }
+            data = GCHandle.ToIntPtr(GCHandle.Alloc(new Measure(parent)));
         }
 
         [DllExport]
@@ -333,5 +402,11 @@ namespace SpotifyPlugin
             return StringBuffer;
         }
 
+        [DllExport]
+        public static void ExecuteBang(IntPtr data, IntPtr args)
+        {
+            Measure measure = (Measure)GCHandle.FromIntPtr(data).Target;
+            measure.ExecuteBang(Marshal.PtrToStringUni(args));
+        }
     }
 }
